4104dd1fb74ff8a53f2115564cb326dcae65d33d
Arc1el <khm970514@gmail.com>
2023-01-20 21:30:06 +0900

    define input, output data type

--- ics_gen/routes/index.js
+++ ics_gen/routes/index.js
@@ -6,13 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
 /*
 ICS Generate Router
 input data = [
-StartDate(yyyyMMdd'T'HHmmss)
-EndDate(yyyyMMdd'T'HHmmss)
-Description(ex. AWS Cloud Bootcamp)
+              StartDate(yyyyMMdd'T'HHmmss) : String
+              EndDate(yyyyMMdd'T'HHmmss) : String
+              Description(ex. AWS Cloud Bootcamp) : String
+            ] : JSON format
+
+ex. {
+  "StartDate" : "20230120T130000",
+  "EndDate" : "20230120T163000",
+  "Description" : "AWS CloudBootCamp OfflineSession"
+}
+
+outputdata = .ics file -> save to local storage
 */
 router.get('/ics_gen', function(req, res, next) {
   res.render('index', { title: 'Express' });
