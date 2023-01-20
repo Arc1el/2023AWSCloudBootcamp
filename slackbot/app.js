7becadb6ff2fe2d889b49f33c87be537870ad83b
Arc1el <khm970514@gmail.com>
2023-01-21 01:17:15 +0900

    use slack-api

--- slackbot/app.js
+++ slackbot/app.js
@@ -3,6 +3,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// for the slack api
+const secrets = require("../secrets/secrets.json")
+const { createEventAdapter } = require("@slack/events-api");
+const { WebClient } = require("@slack/web-api")
+const slackEvents = createEventAdapter("")
+const web = new WebClient(secrets.token);
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -13,6 +20,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// slack api
+slackEvents.on('message', async (event) => {
+    // event hanle
+    const result = await web.chat.postMessage({
+    text: 'text',
+    channel: 'channel',
+    });
+
+    console.log(
+    `Successfully send message ${result.ts} in conversation ${event.channel}`
+    );
+}
+);
+
+slackEvents.on('error', console.error);
+
+app.use('/slack/events', slackEvents.requestListener());
+   // url 유효성 검사를 위한 requestListener
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
