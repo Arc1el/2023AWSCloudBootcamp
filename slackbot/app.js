a9714b9dc1466e67aae1d51d257f2289f8799f10
Arc1el <khm970514@gmail.com>
2023-01-21 13:01:34 +0900

    update public directory for ics file

--- slackbot/app.js
+++ slackbot/app.js
@@ -3,9 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-// for the slack api
-const secrets = require("./secrets/secrets.json");
-var SlackBot = require("slackbots");
+require("dotenv").config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,20 +15,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// slack api
-
-
-bot.on('start', function() {
-    // more information about additional params https://api.slack.com/methods/chat.postMessage
-    var params = {
-        icon_emoji: ':cat:'
-    };
-    
-    // define channel, where bot exist. You can adjust it there https://my.slack.com/services 
-    bot.postMessageToChannel('playground', 'meow!', params);
-
-});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- slackbot/routes/index.js
+++ slackbot/routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const axios = require("axios");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var data = {
+	"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "📅 캘린더에 추가하기 "
+			},
+			"accessory": {
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"text": "Add to Calendar",
+					"emoji": true
+				},
+				"value": "add_calendar_btn",
+				"url": "https://www.phpclasses.org/browse/download/1/file/63438/name/example.ics",
+				"action_id": "button-action"
+			}
+		},
+		{
+			"type": "divider"
+		}
+	]
+}
+
+router.get('/', async function(req, res, next) {
+  axios.post("https://hooks.slack.com/services/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", data
+  ).then(function () {
+    res.send("data successfully sended");
+  }).catch(function () {
+    res.send("error occured");
+  })
 });
 
 module.exports = router;
