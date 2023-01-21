e829b21e2446f8fae110d085a45859cc784b415c
Arc1el <khm970514@gmail.com>
2023-01-21 22:04:41 +0900

    update dataframe to csv

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -4,7 +4,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 from time import sleep
+import pandas as pd
+import numpy as np
 
+# ignore warning message
 warnings.filterwarnings("ignore")
 
 # Initialize Crawler
@@ -16,6 +19,7 @@ options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome("/usr/local/bin/chromedriver")
 driver.implicitly_wait(3)
 
+# crawling target(1)
 url = "https://www.wanted.co.kr/wdlist?country=kr&job_sort=company.response_rate_order&years=-1&locations=all"
 driver.get(url)
 
@@ -23,6 +27,7 @@ html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 items = soup.select("#__next > div.JobList_cn__t_THp > div > div > div.List_List_container__JnQMS > ul > li > div")
 
+# datas(1)
 urls = []
 for item in items:
     url = item.select_one("#__next > div.JobList_cn__t_THp > div > div > div.List_List_container__JnQMS > ul > li > div > a")["href"]
@@ -34,31 +39,57 @@ print()
 
 datas = []
 for url in urls:
+    
+    # crawling target(2)
     driver.get(url)
     #driver.implicitly_wait(30)
-    sleep(3)
+    sleep(1)
 
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
     items = soup.select("#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57")
     for item in items:
         temp = []
-        name = item.select_one("#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57 > section.JobHeader_className__HttDA > div:nth-child(2) > h6 > a").get_text()
-        data = item.select_one("#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57 > div.JobContent_descriptionWrapper__SM4UD > section.JobDescription_JobDescription__VWfcb > p:nth-child(1) > span").get_text()
-        position = soup.select_one("#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57 > section.JobHeader_className__HttDA > h2").get_text()
-        task = soup.select_one("#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57 > div.JobContent_descriptionWrapper__SM4UD > section > p:nth-child(3)").get_text()
-        qualifications = soup.select_one("#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57 > div.JobContent_descriptionWrapper__SM4UD > section > p:nth-child(5)").get_text()
-        prefer = soup.select_one("#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57 > div.JobContent_descriptionWrapper__SM4UD > section.JobDescription_JobDescription__VWfcb > p:nth-child(7)").get_text()
-        welfare = soup.select_one("#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57 > div.JobContent_descriptionWrapper__SM4UD > section.JobDescription_JobDescription__VWfcb > p:nth-child(9)").get_text()
+        # for missing value
+        name, position, description, task, qualifications, prefer, welfare, stack, endDate, location = "","","","","","","","","",""
+        try:
+            # get data
+            name = item.select_one("section.JobHeader_className__HttDA > div:nth-child(2) > h6 > a").get_text()
+            description = item.select_one("div.JobContent_descriptionWrapper__SM4UD > section.JobDescription_JobDescription__VWfcb > p:nth-child(1) > span").get_text()
+            position = soup.select_one("section.JobHeader_className__HttDA > h2").get_text()
+            task = soup.select_one("div.JobContent_descriptionWrapper__SM4UD > section > p:nth-child(3)").get_text()
+            qualifications = soup.select_one("div.JobContent_descriptionWrapper__SM4UD > section > p:nth-child(5)").get_text()
+            prefer = soup.select_one("div.JobContent_descriptionWrapper__SM4UD > section.JobDescription_JobDescription__VWfcb > p:nth-child(7)").get_text()
+            welfare = soup.select_one("div.JobContent_descriptionWrapper__SM4UD > section.JobDescription_JobDescription__VWfcb > p:nth-child(9)").get_text()
+            stack = soup.select_one("div.JobContent_descriptionWrapper__SM4UD > section.JobDescription_JobDescription__VWfcb > p:nth-child(11)").get_text()
+            endDate = soup.select_one("div.JobContent_descriptionWrapper__SM4UD > section.JobWorkPlace_className__ra6rp > div:nth-child(1) > span.body").get_text()
+            location = soup.select_one("div.JobContent_descriptionWrapper__SM4UD > section.JobWorkPlace_className__ra6rp > div:nth-child(2) > span.body").get_text()
+        except:
+            pass
+        finally:
+            # append to datas
+            temp.append(name)
+            temp.append(location)
+            temp.append(position)
+            temp.append(description)
+            temp.append(task)
+            temp.append(qualifications)
+            temp.append(prefer)
+            temp.append(welfare)
+            temp.append(stack)
+            temp.append(endDate)
+            datas.append(temp)
+            
         
-        temp.append(name)
-        temp.append(data)
-        temp.append(position)
-        temp.append(task)
-        temp.append(qualifications)
-        temp.append(prefer)
-        temp.append(welfare)
-        datas.append(temp)
 
 print("Data Length : " + str(len(datas)))
-print(datas)
+
+# convert python array -> numpy array -> pandas dataframe
+datas_narray = np.array(datas)
+dataframe = pd.DataFrame(datas_narray)
+
+# rename columns
+dataframe.columns = ["회사명", "위치", "포지션", "회사소개", "주요업무", "자격요견", "우대사항", "혜택 및 복지", "기술스택", "마감일"]
+
+# save dataframe to csv using utf-8 encording
+dataframe.to_csv("data.csv", mode='w', encoding="utf-8-sig")
