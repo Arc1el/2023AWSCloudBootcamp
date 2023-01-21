64e2b143e1ba90fabd714317d7d4bc2593a816c7
Arc1el <khm970514@gmail.com>
2023-01-21 20:29:30 +0900

    crawl the detail page

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -1,7 +1,11 @@
+import warnings
 from unicodedata import category
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
+from time import sleep
+
+warnings.filterwarnings("ignore")
 
 # Initialize Crawler
 options = webdriver.ChromeOptions()
@@ -19,22 +23,42 @@ html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 items = soup.select("#__next > div.JobList_cn__t_THp > div > div > div.List_List_container__JnQMS > ul > li > div")
 
-#result = []
 urls = []
 for item in items:
-    temp = []
     url = item.select_one("#__next > div.JobList_cn__t_THp > div > div > div.List_List_container__JnQMS > ul > li > div > a")["href"]
-    #companyName = item.select_one("#__next > div.JobList_cn__t_THp > div > div > div.List_List_container__JnQMS > ul > li > div > a")["data-company-name"]
-    #category = item.select_one("#__next > div.JobList_cn__t_THp > div > div > div.List_List_container__JnQMS > ul > li > div > a")["data-job-category"]
-    #position = item.select_one("#__next > div.JobList_cn__t_THp > div > div > div.List_List_container__JnQMS > ul > li > div > a")["data-position-name"]
-    temp.append("https://wanted.co.kr" + url)
-    #temp.append(companyName)
-    #temp.append(category)
-    #temp.append(position)
+    urls.append("https://wanted.co.kr" + url) 
     
-    # result.append(temp)
-    urls.append(temp)
-    # print(temp)
-    
+print("Urls : ")
 print(urls)
-#print(items)
+print()
+
+datas = []
+for url in urls:
+    driver.get(url)
+    #driver.implicitly_wait(30)
+    sleep(3)
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.select("#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57")
+    for item in items:
+        temp = []
+        name = item.select_one("#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57 > section.JobHeader_className__HttDA > div:nth-child(2) > h6 > a").get_text()
+        data = item.select_one("#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57 > div.JobContent_descriptionWrapper__SM4UD > section.JobDescription_JobDescription__VWfcb > p:nth-child(1) > span").get_text()
+        position = soup.select_one("#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57 > section.JobHeader_className__HttDA > h2").get_text()
+        task = soup.select_one("#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57 > div.JobContent_descriptionWrapper__SM4UD > section > p:nth-child(3)").get_text()
+        qualifications = soup.select_one("#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57 > div.JobContent_descriptionWrapper__SM4UD > section > p:nth-child(5)").get_text()
+        prefer = soup.select_one("#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57 > div.JobContent_descriptionWrapper__SM4UD > section.JobDescription_JobDescription__VWfcb > p:nth-child(7)").get_text()
+        welfare = soup.select_one("#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > div.JobDetail_relativeWrapper__F9DT5 > div.JobContent_className___ca57 > div.JobContent_descriptionWrapper__SM4UD > section.JobDescription_JobDescription__VWfcb > p:nth-child(9)").get_text()
+        
+        temp.append(name)
+        temp.append(data)
+        temp.append(position)
+        temp.append(task)
+        temp.append(qualifications)
+        temp.append(prefer)
+        temp.append(welfare)
+        datas.append(temp)
+
+print("Data Length : " + str(len(datas)))
+print(datas)
